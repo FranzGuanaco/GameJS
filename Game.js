1df4c0b9650033fb787b88a5d9b28730a48690dd
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-06-23 19:31:12 +0200

    2

--- Game.js
+++ Game.js
@@ -1,75 +1,112 @@
 var result;
 
-function random(){
-    result = (Math.floor(Math.random()* 6+1));   
-    document.getElementById("des").value=result;
-    total = result + total;
-    document.getElementById("RandomNumb").value = total;
-    
-     if (result===1)
-        {
-            total = 0;
-            document.getElementById("RandomNumb").value= 0;  
-            
-            document.getElementById("Titre").innerHTML = "Au tour de Player 2" ;
-        }     
 
-        return result;    
+function random(){
+    result = (Math.floor(Math.random()* 6+1));  
+   
+    
+   
+     if (result===1)
+        {		total = 0;
+         		result = 0;
+        		
+           
+            
+            document.getElementById("RandomNumb").value= total;  
+            document.getElementById("des").value= 1;
+            document.getElementById("Titre").innerHTML = "Au tour de Player 2" ;
+        }   
+        
+        else {
+         total = result + total;
+     		ttal = result + ttal;
+    
+    document.getElementById("RandomNumb").value = total;
+    document.getElementById("des").value= result
+        
+        }
+
+        return result;
+       
 }
 
 
+
 var total = 0;
-    
+var ttal = 0;
+
 
  function test(){
+
+ total = 0;
     document.getElementById("Titre").innerHTML = "Au tour de Player 2" ;
-    document.getElementById("RandomNumb").value= total;
-    
-    total = result + total;
-    document.getElementById("compteur").innerHTML = total ;
-    
+     document.getElementById("compteur").innerHTML = ttal + total;  /* quelque chose "+" total */
+     
+ 	
+    document.getElementById("RandomNumb").value = total;
+   
+   
+
     if (total>=100)
         {
         document.getElementById("des").value="Vous avez Gagné";
         }    
 
     return total;
+
 }
 
 
-/* -----------------------------Player2----------------------------- */
+/*  PLAYER TWO */
 
 
-        
-var result2 = 0;
+var result2;
+
 
 function random2(){
-    result2 = (Math.floor(Math.random()* 6+1));   
+    result2 = (Math.floor(Math.random()* 6+1));  
+   
     
-    
-    document.getElementById("RandomNumb2").value = result2;
-    
+   
      if (result2===1)
-        {
-            total2 = 0;
-            document.getElementById("compteur2").innerHTML= total2;  
-            document.getElementById("des2").value="Vous avez Perdu";
+        {		total2 = 0;
+         		result2 = 0;
+        		
+           
+            
+            document.getElementById("RandomNumb2").value= total2;  
+            document.getElementById("des2").value= 1;
             document.getElementById("Titre").innerHTML = "Au tour de Player 1" ;
-        }     
+        }   
+        
+        else {
+         total2 = result2 + total2;
+     		ttal2 = result2 + ttal2;
+    
+    document.getElementById("RandomNumb2").value = total2;
+    document.getElementById("des2").value= result2
+        
+        }
 
         return result2;
-        
+       
 }
 
 
 
-var total2 = 1;
+var total2 = 0;
+var ttal2 = 0;
 
 
  function test2(){
+
+ total2 = 0;
     document.getElementById("Titre").innerHTML = "Au tour de Player 1" ;
-    total2 = result2 + total2;
-    document.getElementById("compteur2").innerHTML = total2 ;
+     document.getElementById("compteur2").innerHTML = ttal2 + total2;  
+ 	
+    document.getElementById("RandomNumb2").value = total2;
+   
+   
 
     if (total2>=100)
         {
